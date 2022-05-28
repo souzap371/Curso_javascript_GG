44c803aa0d20a1f7f7ef4f98968337664155d55d
Pablo <souzap371@gmail.com>
2022-05-28 07:44:02 -0300

    Conclusão- exercicio- vamos contar

--- 20script.js
+++ 20script.js
@@ -5,16 +5,32 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[Erro] Faltam dados')
-    }else {
-        res.innerHTML = 'Contando:'
+        //window.alert('[Erro] Faltam dados')
+        res.innerHTML = 'Impossível contar'
+    } else {
+        res.innerHTML = 'Contando: <br>'
 
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for(let c =i; c <= f; c += p ) {
-            res.innerHTML += `${c} `
+        if (p <= 0) {
+            //Verifica se o passo é igual a '0' se Sim, inicia o passo 1
+            window.alert('Passo Inválido! Considerando Passo 1')
+            p = 1
         }
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
